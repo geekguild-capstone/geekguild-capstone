9337747b401cd1438db08bd0e43848fe9de36444
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-17 11:08:30 -0500

    initial setup, added thymeleaf

--- src/main/java/com/geekguild/GeekguildCapstoneApplication.java
+++ src/main/java/com/geekguild/GeekguildCapstoneApplication.java
@@ -1,4 +1,4 @@
-package com.example.geekguildcapstone;
+package com.geekguild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
