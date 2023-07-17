f4ed88ecbe72c6389e0a5ede8205c9995920cdc1
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-17 10:19:13 -0500

    first commit

---
+++ src/main/java/com/example/geekguildcapstone/GeekguildCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.geekguildcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekguildCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekguildCapstoneApplication.class, args);
+	}
+
+}
