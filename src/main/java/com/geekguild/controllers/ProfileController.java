9bc352d470409cc3bc0e22ba246071608ab4edd0
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-18 21:37:15 -0500

    fixed navbar, and portfolio integration

--- src/main/java/com/geekguild/controllers/ProfileController.java
+++ src/main/java/com/geekguild/controllers/ProfileController.java
@@ -1,6 +1,8 @@
 package com.geekguild.controllers;
 
+import com.geekguild.models.Portfolio;
 import com.geekguild.models.User;
+import com.geekguild.repositories.PortfolioRepository;
 import com.geekguild.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sound.sampled.Port;
+
 @Controller
 public class ProfileController {
 
+    private PortfolioRepository portfolioDao;
     private UserRepository userDao;
 
-    public ProfileController(UserRepository userDao) {
+    public ProfileController(UserRepository userDao, PortfolioRepository portfolioDao) {
+        this.portfolioDao = portfolioDao;
         this.userDao = userDao;
     }
 
@@ -34,8 +40,10 @@ public class ProfileController {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.getReferenceById(loggedInUser.getId());
         model.addAttribute("user", user);
+        Portfolio portfolio = portfolioDao.findByUserId(loggedInUser.getId());
+        model.addAttribute("portfolio", portfolio);
 
-        return "/users/profile";
+        return "users/profile";
     }
 
 
@@ -65,10 +73,13 @@ public class ProfileController {
             return "redirect:/error"; // Example: redirect to an error page
         }
 
+        Portfolio portfolio = portfolioDao.findByUserId(user.getId());
+        model.addAttribute("portfolio", portfolio);
+
         // Add the user to the model so it can be accessed in the view
         model.addAttribute("user", user);
 
-        return "/users/profile";
+        return "users/profile";
     }
 
 
