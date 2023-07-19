6d5a1d734eef79693574ecc89635b245f2aa4bf8
Raymond Glass <raymond.allen.glass@gmail.com>
2023-07-19 12:12:47 -0500

    added function to create work and table

--- src/main/java/com/geekguild/controllers/UserController.java
+++ src/main/java/com/geekguild/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.geekguild.controllers;
 
+import com.geekguild.models.Portfolio;
 import com.geekguild.models.User;
+import com.geekguild.models.Work;
+import com.geekguild.repositories.PortfolioRepository;
 import com.geekguild.repositories.UserRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.geekguild.repositories.WorkRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +18,14 @@ public class UserController {
 
 
     private UserRepository userDao;
+    private PortfolioRepository portfolioDao;
+    private WorkRepository workDao;
     private PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userDao, PortfolioRepository portfolioDao, WorkRepository workDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.portfolioDao = portfolioDao;
+        this.workDao = workDao;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -34,7 +40,14 @@ public class UserController {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         userDao.save(user);
-        System.out.println(user);
+        Portfolio portfolio = new Portfolio();
+        portfolio.setUser(user);
+        portfolioDao.save(portfolio);
+        Work work = new Work();
+        work.setUser(user);
+        workDao.save(work);
+
+
         return "redirect:/login";
         //register page login button taking me to about us/homepage.
         //homepage -profile button -GROUPS button -add friend -
