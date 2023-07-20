13577ea0cb24d608190ef3d772e032f42856098a
Consuella Moore <consuellamoore123@gmail.com>
2023-07-20 15:20:48 -0500

    worked on profile styling

--- src/main/java/com/geekguild/repositories/GroupRepository.java
+++ src/main/java/com/geekguild/repositories/GroupRepository.java
@@ -1,8 +1,8 @@
-package com.geekguild.repositories;
-
-import com.geekguild.models.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long>{
-
-}
+////package com.geekguild.repositories;
+//
+//import com.geekguild.models.Group;
+//import org.springframework.data.jpa.repository.JpaRepository;
+////
+////public interface GroupRepository extends JpaRepository<Group, Long>{
+////
+////}
