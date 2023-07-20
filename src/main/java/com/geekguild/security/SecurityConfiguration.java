1feb0cf6398df60a5418eec0166c62cdfee62792
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-20 09:11:20 -0500

    added post photo with submit

--- src/main/java/com/geekguild/security/SecurityConfiguration.java
+++ src/main/java/com/geekguild/security/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
                         /* Pages that do not require authentication
                          * anyone can visit the home page, register, login, and view ads */
 
-                        .requestMatchers("/", "/posts", "/posts/*", "/register", "/login", "/home", "/groups", "/groups/group", "/about-us", "/profile", "/profile/*/edit", "/profile/upload", "/profile/*").permitAll()
+                        .requestMatchers("/", "/posts", "/posts/*", "/register", "/login", "/home", "/groups", "/groups/group", "/about-us", "/profile", "/profile/*/edit", "/profile/upload", "/profile/*", "/home/upload").permitAll()
 
                         // allow loading of static resources
                         .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
