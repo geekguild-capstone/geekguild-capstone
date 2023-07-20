28e1d062edcb0c323bf750c2e953279d327e3fbb
Raymond Glass <raymond.allen.glass@gmail.com>
2023-07-20 11:27:55 -0500

    updated friend model

--- src/main/java/com/geekguild/models/Group.java
+++ src/main/java/com/geekguild/models/Group.java
@@ -1,67 +1,67 @@
-package com.geekguild.models;
-
-
-import jakarta.persistence.*;
-import org.springframework.scheduling.config.Task;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-@Entity
-@Table(name="groups")
-public class Group {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(nullable = false)
-    private String name;
-
-
-    public Group() {
-    }
-
-    public Group(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-    //    Working Group to User Relationship
-    @ManyToMany(mappedBy = "groups")
-    private Set<User> members = new HashSet<>();
-
-
-//    public List<User> getUsers() {
-//        return users;
+//package com.geekguild.models;
+//
+//
+//import jakarta.persistence.*;
+//import org.springframework.scheduling.config.Task;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//
+//@Entity
+//@Table(name="groups")
+//public class Group {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    @Column(nullable = false)
+//    private String name;
+//
+//
+//    public Group() {
 //    }
-
-//    public void setTasks(List<User> users) {
-//        this.users = users;
+//
+//    public Group(String name) {
+//        this.name = name;
 //    }
-
-    //    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
-//    List<User> users = new ArrayList<>();
-
-
-
-}
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//
+//
+//    //    Working Group to User Relationship
+//    @ManyToMany(mappedBy = "groups")
+//    private Set<User> members = new HashSet<>();
+//
+//
+////    public List<User> getUsers() {
+////        return users;
+////    }
+//
+////    public void setTasks(List<User> users) {
+////        this.users = users;
+////    }
+//
+//    //    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
+////    List<User> users = new ArrayList<>();
+//
+//
+//
+//}
