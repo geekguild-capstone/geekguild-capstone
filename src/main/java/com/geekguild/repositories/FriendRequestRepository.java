eb752c7eb1165221cdae1e45fee96d29b25c1ddf
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-23 05:15:38 -0500

    fixed navbar header and footer, added groups endpoint, pulled css out of html

--- src/main/java/com/geekguild/repositories/FriendRequestRepository.java
+++ src/main/java/com/geekguild/repositories/FriendRequestRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
-    // Custom query methods, if needed
     List<FriendRequest> findByReceiverAndStatus(User receiver, String status);
 
     List<FriendRequest> findBySenderAndStatus(User sender, String status);
