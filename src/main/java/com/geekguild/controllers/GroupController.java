3e0977944ae79526a5603457d793469ea9d0b735
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-25 15:27:07 -0500

    added group/{id} front end with working post display

--- src/main/java/com/geekguild/controllers/GroupController.java
+++ src/main/java/com/geekguild/controllers/GroupController.java
@@ -84,17 +84,37 @@ public class GroupController {
         return "redirect:/group/" + groupId;
     }
 
-    @GetMapping("/group")
-    public String showGroup() {
-
-        return "group-card";
-    }
+//    @GetMapping("/group")
+//    public String showGroup() {
+//
+//        return "group";
+//    }
 
     @GetMapping("/group/{id}")
     public String viewGroup(@PathVariable("id") Long groupId, Model model) {
+        model.addAttribute("post", new Post());
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.getReferenceById(loggedInUser.getId());
+        model.addAttribute("user", user);
+//        List<Post> posts = postDao.findAll();
+//        model.addAttribute("posts", posts);
+        List<Post> groupPosts = postDao.findByGroupId(groupId);
+        model.addAttribute("groupPosts", groupPosts);
+        List<User> users = userDao.findAll();
+        model.addAttribute("users", users);
+        List<FriendRequest> receiveFriends = friendDao.findByReceiverAndStatus(user, "accepted");
+        model.addAttribute("receiveFriends", receiveFriends);
+        List<FriendRequest> sentFriends = friendDao.findBySenderAndStatus(user, "accepted");
+        model.addAttribute("sentFriends", sentFriends);
+        // Fetch the list of users who are not friends with the logged-in user
+        List<User> usersNotFriendsWithLoggedInUser = userDao.findUsersNotFriendsWithAndNotPending(user.getId());
+        model.addAttribute("notFriends", usersNotFriendsWithLoggedInUser);
         //Retrieve the group with the giver Id from the database
         Group group = groupDao.findById(groupId).orElse(null);
         model.addAttribute("group", group);
+        List<Group> groups = groupDao.findAll();
+        model.addAttribute("groups", groups);
+
 
 
 //         Check if the group exists
--- src/main/java/com/geekguild/repositories/PostRepository.java
+++ src/main/java/com/geekguild/repositories/PostRepository.java
@@ -3,7 +3,11 @@ package com.geekguild.repositories;
 import com.geekguild.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long>{
+    List<Post> findByGroupId(Long groupId);
+
 
 
 
