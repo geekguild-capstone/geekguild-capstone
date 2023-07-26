db1cc81fe9e922d597598a8b6b60d2d18be364e8
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-26 13:46:12 -0500

    starting to wire up comments with posts

--- src/main/java/com/geekguild/controllers/HomeController.java
+++ src/main/java/com/geekguild/controllers/HomeController.java
@@ -75,12 +75,42 @@ public class HomeController {
         return "redirect:/home";
     }
 
+//    @PostMapping("/home/remove")
+//    public String removeFriend(@RequestParam("friendId") Long friendId, @RequestParam("loggedInUserType") String loggedInUserType) {
+//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User friend = userDao.findById(friendId).orElse(null);
+//
+//        if (friend != null) {
+//            FriendRequest friendRequest;
+//            if ("sender".equals(loggedInUserType)) {
+//                friendRequest = friendDao.findBySenderAndReceiverAndStatus(loggedInUser, friend, "accepted");
+//            } else if ("receiver".equals(loggedInUserType)) {
+//                friendRequest = friendDao.findBySenderAndReceiverAndStatus(friend, loggedInUser, "accepted");
+//            } else {
+//                return "redirect:/home";
+//            }
+//
+//            if (friendRequest != null) {
+//                friendRequest.setStatus("rejected");
+//                friendDao.save(friendRequest);
+//            }
+//        }
+//
+//        return "redirect:/home";
+//    }
+
     @PostMapping("/home/remove")
     public String removeFriend(@RequestParam("friendId") Long friendId, @RequestParam("loggedInUserType") String loggedInUserType) {
-        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        // Get the logged-in user ID from the SecurityContextHolder
+        Long loggedInUserId = getUserIdFromSecurityContext();
+
+        // Fetch the logged-in user from the database
+        User loggedInUser = userDao.findById(loggedInUserId).orElse(null);
+
+        // Fetch the friend to be removed from the database
         User friend = userDao.findById(friendId).orElse(null);
 
-        if (friend != null) {
+        if (loggedInUser != null && friend != null) {
             FriendRequest friendRequest;
             if ("sender".equals(loggedInUserType)) {
                 friendRequest = friendDao.findBySenderAndReceiverAndStatus(loggedInUser, friend, "accepted");
@@ -98,136 +128,14 @@ public class HomeController {
 
         return "redirect:/home";
     }
+
+    // Helper method to get the logged-in user ID from the SecurityContextHolder
+    private Long getUserIdFromSecurityContext() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            return ((User) principal).getId();
+        }
+        return null;
+    }
 }
 
-
-
-
-
-//package com.geekguild.controllers;
-//
-//import com.geekguild.models.FriendRequest;
-//import com.geekguild.models.Post;
-//import com.geekguild.models.User;
-//import com.geekguild.repositories.FriendRequestRepository;
-//import com.geekguild.repositories.PostRepository;
-//import com.geekguild.repositories.UserRepository;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.ui.Model;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@Controller
-//public class HomeController {
-//
-//    private UserRepository userDao;
-//    private PostRepository postDao;
-//    private FriendRequestRepository friendDao;
-//
-//
-//    public HomeController(UserRepository userDao, PostRepository postDao, FriendRequestRepository friendDao) {
-//        this.userDao = userDao;
-//        this.postDao = postDao;
-//        this.friendDao = friendDao;
-//    }
-//
-//    @GetMapping("/home")
-//    public String landingPage(Model model) {
-//        model.addAttribute("post", new Post());
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User user = userDao.getReferenceById(loggedInUser.getId());
-//        model.addAttribute("user", user);
-//        List<Post> posts = postDao.findAll();
-//        model.addAttribute("posts", posts);
-//        List<User> users = userDao.findAll();
-//        model.addAttribute("users", users);
-//        List<FriendRequest> receiveFriends = friendDao.findByReceiverAndStatus(user, "accepted");
-//        model.addAttribute("receiveFriends", receiveFriends);
-//        List<FriendRequest> sentFriends = friendDao.findBySenderAndStatus(user, "accepted");
-//        model.addAttribute("sentFriends", sentFriends);
-//        // Fetch the list of users who are not friends with the logged-in user
-//        List<User> usersNotFriendsWithLoggedInUser = userDao.findUsersNotFriendsWithAndNotPending(user.getId());
-//        model.addAttribute("notFriends", usersNotFriendsWithLoggedInUser);
-//
-//
-//
-//        return "users/home";
-//    }
-//
-//    @GetMapping("/about-us")
-//    public String showAbout() {
-//
-//        return "/partials/about-us";
-//    }
-//
-//    @PostMapping("/home")
-//    public String showCreatePostForm(@ModelAttribute Post post, @RequestParam("image") String image) {
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        post.setUser(loggedInUser);
-//
-//        // Set the image URL from the request parameter directly in the post entity
-//        post.setImage(image);
-//
-//        postDao.save(post);
-//        return "redirect:/home";
-//    }
-//
-//    @PostMapping("/home/add")
-//    public String sendFriendRequest(@RequestParam("receiverId") Long receiverId) {
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User receiver = userDao.findById(receiverId).orElse(null);
-//
-//        if (receiver != null) {
-//            // Create a new FriendRequest and set the sender, receiver, and status
-//            FriendRequest friendRequest = new FriendRequest();
-//            friendRequest.setSender(loggedInUser);
-//            friendRequest.setReceiver(receiver);
-//            friendRequest.setStatus("pending"); // You can set the initial status as "pending" or any other value
-//
-//            // Save the friend request to the database
-//            friendDao.save(friendRequest);
-//        }
-//
-//        return "redirect:/home"; // Redirect back to the home page after sending the friend request
-//    }
-//
-//
-//    @PostMapping("/home/remove")
-//    public String removeFriend(@RequestParam("friendId") Long friendId, @RequestParam("loggedInUserType") String loggedInUserType) {
-//        System.out.println("Endpoint hit");
-//
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User friend = userDao.findById(friendId).orElse(null);
-//
-//        if (friend != null) {
-//            if ("sender".equals(loggedInUserType)) {
-//                // If the logged-in user is the sender of the friend request
-//                FriendRequest friendRequest = friendDao.findBySenderAndReceiverAndStatus(loggedInUser, friend, "accepted");
-//                if (friendRequest != null) {
-//                    // Set the status of the friend request to "rejected"
-//                    friendRequest.setStatus("rejected");
-//                    friendDao.save(friendRequest);
-//                    System.out.println("Friend request status set to 'rejected' for sender"); // Add this line for debugging
-//                }
-//            } else if ("receiver".equals(loggedInUserType)) {
-//                // If the logged-in user is the receiver of the friend request
-//                FriendRequest friendRequest = friendDao.findBySenderAndReceiverAndStatus(friend, loggedInUser, "accepted");
-//                if (friendRequest != null) {
-//                    // Set the status of the friend request to "rejected"
-//                    friendRequest.setStatus("rejected");
-//                    friendDao.save(friendRequest);
-//                    System.out.println("Friend request status set to 'rejected' for receiver"); // Add this line for debugging
-//                }
-//            }
-//        }
-//
-//        return "redirect:/home";
-//    }
-//
-//
-//}
