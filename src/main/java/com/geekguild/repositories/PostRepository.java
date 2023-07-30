1f165d1eaaa9f31ac473b99aae483c7f8fae04b2
Theodoric Graham <theodoricgraham@gmail.com>
2023-07-30 16:25:27 -0500

    delete functionality on posts, with conditional display if you are the post owner

--- src/main/java/com/geekguild/repositories/PostRepository.java
+++ src/main/java/com/geekguild/repositories/PostRepository.java
@@ -3,14 +3,19 @@ package com.geekguild.repositories;
 import com.geekguild.models.Post;
 import com.geekguild.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
     List<Post> findByGroupId(Long groupId);
 
-
     List<Post> findByGroupIdIsNull();
 
     List<Post> findByGroupIdAndUser(Long groupId, User user);
+
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.id = :postId")
+    void deletePostById(Long postId);
 }
