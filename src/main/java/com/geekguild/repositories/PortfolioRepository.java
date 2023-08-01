776799ba98e5db3865feb45daad08ff054911708
Theodoric Graham <theodoricgraham@gmail.com>
2023-08-01 00:52:23 -0500

    small changes to controllers and repositories

--- src/main/java/com/geekguild/repositories/PortfolioRepository.java
+++ src/main/java/com/geekguild/repositories/PortfolioRepository.java
@@ -4,9 +4,11 @@ package com.geekguild.repositories;
 import com.geekguild.models.Portfolio;
 import com.geekguild.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
-    Portfolio findByUserId(Long id);
+    @Query("SELECT p FROM Portfolio p WHERE p.user.id = :userId")
+    Portfolio findByUserId(Long userId);
 
 }
